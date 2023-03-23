7020072d47e6ba556cc116a032b0ffa1374d72e2
almazkg <128687072+almazkg@users.noreply.github.com>
2023-03-23 15:27:53 +0600

    Delete venv directory

--- venv/codes.py
+++
@@ -1,4 +0,0 @@
-file = open('file.txt', 'w', encoding='UTF-8')
-file.write('Бишкек, Кыргызстан')
-file.close()
-
--- venv/lesson 6.py
+++
@@ -1,6 +0,0 @@
-#функции аргументы:*args,**kwargs
-#dry - dont try yourself
-
-# def- definition все вункции через def
- # min max
-
